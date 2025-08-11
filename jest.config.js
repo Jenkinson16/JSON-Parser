48c87f42f178780adf6a9cfcbfae3de7827dd867
Jenkinson W <163100349+Jenkinson16@users.noreply.github.com>
2025-08-11 11:44:34 +0530

    Files are up-to-date

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', 
   },
 };
